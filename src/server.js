f360859186b69be56ccf231dd3a4464c4e920b74
yashPundhir <yash.pundhir.prof@gmail.com>
2025-03-13 23:41:10 +0530

    add new route

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+app.get("/home", (req, res) => {
+	res.send("This is home route");
+});
+
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
