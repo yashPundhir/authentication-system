12507af9aa001d8a26cf9e5f9db66ef2448e5dde
yashPundhir <yash.pundhir.prof@gmail.com>
2025-05-14 08:03:43 +0530

    write auth midleware to verify jsw token in user's cookies

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+
+export const isLoggedIn = async (req, res, next) => {
+	try {
+		console.log(req.cookies);
+
+		const token = req.cookies?.token;
+
+		console.log("token found: ", token ? "YES" : "NO");
+
+		if (!token) {
+			console.log("No token found");
+
+			return res.status(401).json({
+				success: false,
+				message: "Authentication Failed",
+			});
+		}
+
+		const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+		console.log("decoded data: ", decodedToken);
+
+		req.user = decodedToken;
+
+		next();
+	} catch (error) {
+		console.log("Auth Middleware Failure");
+
+		console.log(error);
+
+		return res.status(500).json({
+			success: false,
+			message: "Internal Server Error",
+		});
+	}
+
+	next();
+};
